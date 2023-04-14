4d92533b4379a70ae097966fe630c4a5e992f1b9
liqikai <li_qi_kai@qq.com>
2023-04-14 12:33:52 +0800

    add vel control

--- script/lion_joy.py
+++ script/lion_joy.py
@@ -5,6 +5,8 @@ from sensor_msgs.msg import Joy
 from geometry_msgs.msg import Twist
 from ocs2_msgs.msg import mode_schedule
 from geometry_msgs.msg import PoseStamped
+from ocs2_msgs.msg import mpc_observation
+import tf
 import math
 
 # LB: button 6
@@ -29,7 +31,7 @@ mode_publisher = rospy.Publisher('legged_robot_mpc_mode_schedule',mode_schedule,
 target_publisher = rospy.Publisher('/move_base_simple/goal', PoseStamped, queue_size=10)
 tw = Twist()
 
-
+init_flag = True
 stand_up_enter_flag = False
 stand_up_flag = False
 sit_down_enter_flag = False
@@ -37,6 +39,11 @@ sit_down_flag = True
 stance_flag = True
 trot_flag = False
 gait_switch_flag = False
+
+x_obs = 0.0
+y_obs = 0.0
+z_obs = 0.0
+rpy_obs = [0.0,0.0,0.0]
 
 static_walk = mode_schedule()
 static_walk.eventTimes = [0.0,0.3,0.6,0.9,1.2]
@@ -76,21 +83,21 @@ last_total_vel = 0.0
 
 def callback_joy(data:Joy):
     # print(data.axes[0]) 
-    global stand_up_enter_flag
-    global sit_down_enter_flag
+    global stand_up_enter_flag,stand_up_flag
+    global sit_down_enter_flag,sit_down_flag
     global trot_flag,stance_flag,gait_switch_flag
-    if(data.buttons[LB]==1 and sit_down_flag):
+    if(data.buttons[LB]==1 and sit_down_flag and stance_flag):
         print("LB")
         stand_up_enter_flag = True
-    elif(data.buttons[LB]==1 and stand_up_flag):
+    elif(data.buttons[LB]==1 and stand_up_flag and stance_flag):
         print("LB")
         sit_down_enter_flag = True
     #if RB button was pressed, switch between stance and trot
-    if(data.buttons[RB]==1 and stance_flag):
+    if(data.buttons[RB]==1 and stance_flag and stand_up_flag):
         trot_flag=True
         stance_flag=False
         gait_switch_flag=True
-    elif(data.buttons[RB]==1 and trot_flag):
+    elif(data.buttons[RB]==1 and trot_flag and stand_up_flag):
         stance_flag=True
         trot_flag=False
         gait_switch_flag=True
@@ -99,16 +106,51 @@ def callback_joy(data:Joy):
     tw.linear.y = 0.5*data.axes[0]
     tw.angular.z = data.axes[2]
 
+def callback_state(data:mpc_observation):
+    global x_obs,y_obs,z_obs,rpy_obs
+    global sit_down_flag,stand_up_flag,init_flag
+    x_obs = data.state.value[6]
+    y_obs = data.state.value[7]    
+    z_obs = data.state.value[8]
+    rpy_obs[0] = data.state.value[9]
+    if(init_flag):
+        if(z_obs<0.1):
+            sit_down_flag = True
+            stand_up_flag = False
+        elif(z_obs>0.4):
+            sit_down_flag = False
+            stand_up_flag = True
+        init_flag = False
+    # rpy_obs[1] = data.state.value[10]
+    # rpy_obs[2] = data.state.value[11]
+    # print(rpy_obs)
+
 
 if __name__ == '__main__':
     try:
         rospy.init_node('lion_joy')
-        sub = rospy.Subscriber('joy',Joy,callback_joy)
+        joy_subscriber = rospy.Subscriber('joy',Joy,callback_joy)
+        state_subscriber = rospy.Subscriber('legged_robot_mpc_observation',mpc_observation,callback_state)
         rate = rospy.Rate(10)
         while not rospy.is_shutdown():
-            if stand_up_enter_flag and not stand_up_flag:
+            #TODO get current pose
+
+            if stand_up_enter_flag and not stand_up_flag and stance_flag:
                 print("standing up")
                 sit_down_pose.pose.position.z = sit_down_pose.pose.position.z + 0.05
+                sit_down_pose.pose.position.x = x_obs
+                sit_down_pose.pose.position.y = y_obs
+                #rpy_obs to quaternion
+                #restrict rpy_obs[0] to be in [0,2*pi]
+                if rpy_obs[0] < 0:
+                    rpy_obs[0] = rpy_obs[0] + 2*math.pi
+                elif rpy_obs[0] > 2*math.pi:
+                    rpy_obs[0] = rpy_obs[0] - 2*math.pi
+                quat = tf.transformations.quaternion_from_euler(rpy_obs[0],rpy_obs[1],rpy_obs[2])
+                sit_down_pose.pose.orientation.x = quat[0]
+                sit_down_pose.pose.orientation.y = quat[1]
+                sit_down_pose.pose.orientation.z = quat[2]
+                sit_down_pose.pose.orientation.w = quat[3]
                 target_publisher.publish(sit_down_pose)
                 if sit_down_pose.pose.position.z >= 0.5:
                     stand_up_enter_flag = False
@@ -116,9 +158,16 @@ if __name__ == '__main__':
                     sit_down_flag = False
                     sit_down_pose.pose.position.z = 0.0
                     print("stand up finished")
-            elif sit_down_enter_flag and not sit_down_flag:
+            elif sit_down_enter_flag and not sit_down_flag and stance_flag:
                 print("sitting down")
                 stand_up_pose.pose.position.z = stand_up_pose.pose.position.z - 0.05
+                stand_up_pose.pose.position.x = x_obs
+                stand_up_pose.pose.position.y = y_obs
+                quat = tf.transformations.quaternion_from_euler(rpy_obs[0],rpy_obs[1],rpy_obs[2])
+                stand_up_pose.pose.orientation.x = quat[0]
+                stand_up_pose.pose.orientation.y = quat[1]
+                stand_up_pose.pose.orientation.z = quat[2]
+                stand_up_pose.pose.orientation.w = quat[3]
                 target_publisher.publish(stand_up_pose)
                 if stand_up_pose.pose.position.z <= 0.0:
                     sit_down_enter_flag = False
@@ -128,21 +177,16 @@ if __name__ == '__main__':
                     print("sit down finished")
 
             if trot_flag and gait_switch_flag:
-                print("trot")
+                print("trot mode")
                 mode_publisher.publish(trot)
                 gait_switch_flag=False
             elif stance_flag and gait_switch_flag:
-                print("stance")
+                print("stance mode")
                 mode_publisher.publish(stance)
                 gait_switch_flag=False
 
-            # total_vel = math.sqrt(tw.linear.x**2+tw.linear.y**2+tw.angular.z**2)
-            # if total_vel>0.03 and last_total_vel<=0.03:
-            #     mode_publisher.publish(trot)
-            # elif total_vel<0.03 and last_total_vel>=0.03:
-            #     mode_publisher.publish(stance)
-            # last_total_vel = total_vel
-            # vel_publisher.publish(tw) 
+            if(trot_flag):
+                vel_publisher.publish(tw) 
             rate.sleep()
     except rospy.ROSInterruptException:
         pass
