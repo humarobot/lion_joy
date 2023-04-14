2cac8f464a4b93edac5d796d9c267439af87624e
liqikai <li_qi_kai@qq.com>
2023-04-14 09:12:36 +0800

    add stand up and sit down procedure

--- script/lion_joy.py
+++ script/lion_joy.py
@@ -4,11 +4,36 @@ import rospy
 from sensor_msgs.msg import Joy
 from geometry_msgs.msg import Twist
 from ocs2_msgs.msg import mode_schedule
+from geometry_msgs.msg import PoseStamped
 import math
 
-pub = rospy.Publisher('cmd_vel', Twist, queue_size=10)
-mode_pub = rospy.Publisher('legged_robot_mpc_mode_schedule',mode_schedule, queue_size=10)
+# LB: button 6
+# RB: button 7
+# LT: button 8
+# RT: button 9
+# A: button 0
+# B: button 1
+# X: button 3
+# Y: button 4
+LB= 6
+RB= 7
+LT= 8
+RT= 9
+A= 0
+B= 1
+X= 3
+Y= 4
+
+vel_publisher = rospy.Publisher('cmd_vel', Twist, queue_size=10)
+mode_publisher = rospy.Publisher('legged_robot_mpc_mode_schedule',mode_schedule, queue_size=10)
+target_publisher = rospy.Publisher('/move_base_simple/goal', PoseStamped, queue_size=10)
 tw = Twist()
+
+
+stand_up_enter_flag = False
+stand_up_flag = False
+sit_down_enter_flag = False
+sit_down_flag = True
 
 static_walk = mode_schedule()
 static_walk.eventTimes = [0.0,0.3,0.6,0.9,1.2]
@@ -22,12 +47,40 @@ stance = mode_schedule()
 stance.eventTimes = [0.0,0.5]
 stance.modeSequence = [15]
 
+sit_down_pose = PoseStamped()
+sit_down_pose.header.frame_id = "odom"
+sit_down_pose.pose.position.x = 0.0
+sit_down_pose.pose.position.y = 0.0
+sit_down_pose.pose.position.z = 0.0
+sit_down_pose.pose.orientation.x = 0.0
+sit_down_pose.pose.orientation.y = 0.0
+sit_down_pose.pose.orientation.z = 0.0
+sit_down_pose.pose.orientation.w = 1.0
+
+stand_up_pose = PoseStamped()
+stand_up_pose.header.frame_id = "odom"
+stand_up_pose.pose.position.x = 0.0
+stand_up_pose.pose.position.y = 0.0
+stand_up_pose.pose.position.z = 0.5
+stand_up_pose.pose.orientation.x = 0.0
+stand_up_pose.pose.orientation.y = 0.0
+stand_up_pose.pose.orientation.z = 0.0
+stand_up_pose.pose.orientation.w = 1.0
+
 total_vel = 0.0
 last_total_vel = 0.0
 
 
-def callback(data:Joy):
+def callback_joy(data:Joy):
     # print(data.axes[0]) 
+    global stand_up_enter_flag
+    global sit_down_enter_flag
+    if(data.buttons[LB]==1 and sit_down_flag):
+        print("LB")
+        stand_up_enter_flag = True
+    elif(data.buttons[LB]==1 and stand_up_flag):
+        print("LB")
+        sit_down_enter_flag = True
     tw.linear.x = 0.5*data.axes[1]
     tw.linear.y = 0.5*data.axes[0]
     tw.angular.z = data.axes[2]
@@ -36,18 +89,39 @@ def callback(data:Joy):
 if __name__ == '__main__':
     try:
         rospy.init_node('lion_joy')
-        sub = rospy.Subscriber('joy',Joy,callback)
+        sub = rospy.Subscriber('joy',Joy,callback_joy)
         rate = rospy.Rate(10)
         while not rospy.is_shutdown():
-            total_vel = math.sqrt(tw.linear.x**2+tw.linear.y**2+tw.angular.z**2)
+            if stand_up_enter_flag and not stand_up_flag:
+                print("standing up")
+                sit_down_pose.pose.position.z = sit_down_pose.pose.position.z + 0.05
+                target_publisher.publish(sit_down_pose)
+                if sit_down_pose.pose.position.z >= 0.5:
+                    stand_up_enter_flag = False
+                    stand_up_flag = True
+                    sit_down_flag = False
+                    sit_down_pose.pose.position.z = 0.0
+                    print("stand up finished")
+            elif sit_down_enter_flag and not sit_down_flag:
+                print("sitting down")
+                stand_up_pose.pose.position.z = stand_up_pose.pose.position.z - 0.05
+                target_publisher.publish(stand_up_pose)
+                if stand_up_pose.pose.position.z <= 0.0:
+                    sit_down_enter_flag = False
+                    sit_down_flag = True
+                    stand_up_flag = False
+                    stand_up_pose.pose.position.z = 0.5
+                    print("sit down finished")
 
-            if total_vel>0.03 and last_total_vel<=0.03:
-                mode_pub.publish(trot)
-            elif total_vel<0.03 and last_total_vel>=0.03:
-                mode_pub.publish(stance)
-            last_total_vel = total_vel
+            # total_vel = math.sqrt(tw.linear.x**2+tw.linear.y**2+tw.angular.z**2)
 
-            pub.publish(tw) 
+            # if total_vel>0.03 and last_total_vel<=0.03:
+            #     mode_publisher.publish(trot)
+            # elif total_vel<0.03 and last_total_vel>=0.03:
+            #     mode_publisher.publish(stance)
+            # last_total_vel = total_vel
+
+            # vel_publisher.publish(tw) 
             rate.sleep()
     except rospy.ROSInterruptException:
         pass
